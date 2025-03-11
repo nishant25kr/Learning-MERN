4404b30e44f5d25d21da1209039fb81728f77151
Nishant kumar <nishant927472@gmail.com>
2025-03-11 17:56:49 +0530

    two sum problem

--- DSA/binarysearchtree.cpp
+++ DSA/binarysearchtree.cpp
@@ -1,100 +1,145 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-struct Node {
+struct Node
+{
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
-    Node(int data) {
-        this->data = data;  // Corrected initialization
+    Node(int data)
+    {
+        this->data = data; // Corrected initialization
         left = nullptr;
         right = nullptr;
     }
 };
 
 // Function to insert a new node into the BST
-Node* insert(Node* root, int value) {
-    if (root == nullptr) {
+Node *insert(Node *root, int value)
+{
+    if (root == nullptr)
+    {
         return new Node(value);
     }
-    if (value < root->data) {
+    if (value < root->data)
+    {
         root->left = insert(root->left, value);
-    } else {
+    }
+    else
+    {
         root->right = insert(root->right, value);
     }
     return root;
 }
 
+// deleting the node
 
-//deleting the node
-
-Node* findlastright(Node* root){
-    if(root->right == NULL){
+Node *findlastright(Node *root)
+{
+    if (root->right == NULL)
+    {
         return root;
     }
     return findlastright(root->right);
 }
 
-Node* Helper(Node* root){
+Node *Helper(Node *root)
+{
 
-    if(root->left == NULL){
+    if (root->left == NULL)
+    {
         return root->right;
     }
-    if(root->right == NULL){
+    if (root->right == NULL)
+    {
         return root->left;
     }
-    Node* right=root->right;
-    Node* lastright = findlastright(root->left);
-    lastright->right=right;
+    Node *right = root->right;
+    Node *lastright = findlastright(root->left);
+    lastright->right = right;
 
     return root->left;
-
 }
 
+// Node *deletenode(Node *root, int val)
+// {
+//     if (root == NULL)
+//     {
+//         return NULL;
+//     }
+//     if (root->data == val)
+//     {
+//         return Helper(root);
+//     }
+//     while (root)
+//     {
+//         if (root->data > val)
+//         {
+//             if (root->left != NULL and root->left->data == val)
+//             {
+//                 root->left = Helper(root->left);
+//                 break;
+//             }
+//             else
+//             {
+//                 root = root->left;
+//             }
+//         }
+//         else
+//         {
+//             if (root->right != NULL and root->right->data == val)
+//             {
+//                 root->right = Helper(root->right);
+//                 break;
+//             }
+//             else
+//             {
+//                 root = root->right;
+//             }
+//         }
+//     }
+// }
 
-Node* deletenode(Node* root,int val){
-    if(root == NULL){
-        return NULL;
-    }
-    if(root->data == val){
-        return Helper(root);
-    }
-    while (root)
+
+
+
+void inorderts(Node *root, vector<int> &res)
+{
+    if (root != nullptr)
     {
-        if (root->data > val)
-        {
-            if (root->left != NULL and root->left->data == val)
-            {
-                root->left= Helper(root->left);
-                break;
-            }else{
-                root=root->left;
-            }
-        }else{
-            if (root->right != NULL and root->right->data == val)
-            {
-                root->right = Helper(root->right);
-                break;
-            }else{
-                root=root->right;
-            }
-        }
+        inorderts(root->left, res);  
+        res.push_back(root->data);   
+        inorderts(root->right, res); 
     }
 }
 
 
-// Function to perform inorder traversal of BST (Left, Root, Right)
-void inorder(Node* root) {
-    if (root != nullptr) {
-        cout << root->data << " ";
-        inorder(root->left);
-        inorder(root->right);
+
+bool two_sum(Node *root, int k)
+{
+    vector<int> res;
+    inorderts(root, res); 
+
+    int left = 0, right = res.size() - 1;
+    while (left < right)
+    {
+        int sum = res[left] + res[right];
+        if (sum == k)
+            return 1;
+        else if (sum < k)
+            left++;
+        else
+            right--;
     }
+    return 0;
 }
 
-int main() {
-    Node* root = nullptr;
+
+int main()
+{
+    Node *root = nullptr;
     root = insert(root, 4);
     insert(root, 2);
     insert(root, 6);
@@ -103,17 +148,15 @@ int main() {
     insert(root, 5);
     insert(root, 7);
 
-    cout << "Inorder Traversal of BST: ";
-    inorder(root);
-    cout << endl;
+    // cout << "Inorder Traversal of BST: ";
+    // inorder(root);
+    // cout << endl;
 
-    cout<<"BST after deleting the node";
-    deletenode(root,3);
-    inorder(root);
-    cout << endl;
+    // cout<<"BST after deleting the node";
+    // deletenode(root,3);
+    // inorder(root);
 
-
-
+    cout<<two_sum(root, 4);
 
     return 0;
 }
