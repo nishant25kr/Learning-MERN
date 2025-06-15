af917a12b6731076be13bc1a7c0f5e97b7919bc0
Nishant kumar <nishant927472@gmail.com>
2025-06-15 14:24:58 +0530

    array and math

---
+++ JAVASCRIPT/Chai aur code/Basics/web10-array.js
@@ -0,0 +1,21 @@
+const arr = [1,2,3,4,5]
+// const arr = [1,2,3,4,5,true,"nishant"]
+
+// console.log(arr)
+
+// arr.unshift(100)
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+
+// console.log(arr.length)
+// const newArr = arr.join()
+// console.log(newArr)
+  
+// console.log(newArr.length)
+
+// slice and spice 
+console.log(arr.slice(1,3))
+console.log(arr)
+console.log(arr.splice(1,3))
+console.log(arr)
---
+++ JAVASCRIPT/Chai aur code/Basics/web9-mathandnums.js
@@ -0,0 +1,17 @@
+const score = 1000;
+
+const balance = new Number(100)
+const arr= [1,2,3,4,5,6]
+
+// console.log(score)
+// console.log(balance)
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.random())
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+// console.log(Math.min(arr))
+
+let randomNumber=Math.floor((Math.random()*10) + 1)
+console.log(randomNumber)
+
