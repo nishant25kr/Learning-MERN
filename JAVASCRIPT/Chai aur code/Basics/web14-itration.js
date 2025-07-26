fd424ac6fa3521c6adadc7bf3d4f4f37bbc11712
nishant25kr <nishant927472@gmail.com.com>
2025-07-26 15:43:32 +0530

    learn about itration, DOM

--- JAVASCRIPT/Chai aur code/Basics/web14-itration.js
+++ JAVASCRIPT/Chai aur code/Basics/web14-itration.js
@@ -1,5 +1,23 @@
-for (let i = 0; i < 10; i++) {
-    const element = i;
-    console.log(element)
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element)
 
+// }
+
+// const greeting = "Nishant kumar"
+// for(const val of greeting){
+//     console.log(`char is ${val}`);
+// }
+
+//Maps
+
+const map = new Map()
+
+map.set('IN',"INDIA")
+map.set('US',"USA")
+map.set('CH',"china")
+
+for (const [key,val] of map){
+    console.log("Key is", key, "value is",val);
 }
+
