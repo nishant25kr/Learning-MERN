e14796425457d8eb5ada7c6837673537115d4a60
nishant25kr <nishant927472@gmail.com>
2025-08-03 14:09:49 +0530

    learn classes fetch and oops

---
+++ JAVASCRIPT/Chai aur code/Advance/Classes/myClasses.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(username,email,password) {
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+    encryptpassword(){
+        return `${this.password}12345`
+    };
+    callme(){
+        return `${this.username.toUpperCase()}`
+    };
+}
+
+const Nishant = new User("Nishant","nishant@gmail.com","nishant123")
+
+console.log(Nishant.encryptpassword())
+console.log(Nishant.callme())
---
+++ JAVASCRIPT/Chai aur code/Advance/One/oops.js
@@ -0,0 +1,6 @@
+//  #java script and classes
+
+// #OOP --> programming paradime, way to writing the code
+
+// #object --> collection of properties and method
+
