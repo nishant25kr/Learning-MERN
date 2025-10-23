30102290833d616bdae57a58e8f485e74a8bb4f8
nishant25kr <nishant927472@gmail.com>
2025-10-23 13:36:55 +0530

    Refactor User function and add inner function to demonstrate scope

--- JAVASCRIPT/Chai aur code/Advance/Classes/myClasses.js
+++ JAVASCRIPT/Chai aur code/Advance/Classes/myClasses.js
@@ -20,7 +20,32 @@ function User(name,email,password){
 
 const Nishant = new User("Nishant","nishant@gmail.com","nishant123")
 
-console.log(Nishant)
+// console.log(Nishant)
 // console.log(Nishant.encryptpassword())
 // console.log(Nishant.callme())
 // console.log(Nishant.SeeUsername)
+
+
+// console.log(0 == "0");    //0      
+// console.log(false == "false"); //0 
+// console.log(false == "0");      //0
+// console.log(null == undefined); //0
+// console.log([] == "");          //1
+// console.log([] == 0);           //1
+// console.log([0] == 0);          //1
+// console.log([1] == true);       //1
+// console.log([1,2] == "1,2");    //1
+// console.log([1,2] == "1,2,3");  //0  "1,2"!="1,2,3"
+
+// console.log("1"==="a")
+
+
+function outer(){
+    console.log("hello outer")
+    function inner(){
+        console.log("inner")
+    }
+    inner()
+}
+
+outer()
