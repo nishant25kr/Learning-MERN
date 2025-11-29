8c8a7f27495b0cdffcfbeee455d8b474df1c1c52
nishant25kr <nishant927472@gmail.com>
2025-11-29 15:45:04 +0530

    Add lower_bound and upper_bound functions to binary search implementation

--- DSA/Binary_search.cpp
+++ DSA/Binary_search.cpp
@@ -19,9 +19,50 @@ bool BinarySearch_int(vector<int> &nums, int target){
     
 }
 
+int lower_bound(vector<int > &nums, int target){
+
+    int low = 0;
+    int high = nums.size()-1;
+    int ans = nums.size()-1;
+    while(low <= high){
+        int mid = (high + low) / 2;
+        if(nums[mid] >= target){
+            ans = mid;
+            high = mid-1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    return ans;
+
+}
+
+int upper_bound(vector<int > &nums, int target){
+    int low = 0;
+    int high = nums.size()-1;
+    int ans;
+    while(low <= high){
+        int mid = (high + low) / 2;
+        if (nums[mid] <= target)
+        {
+            ans = mid;
+            low = mid+1;
+        }else{
+            high = mid - 1;
+        }
+        
+    }
+    return ans;
+}
+
 int main(){
 
-    vector<int > nums = {-1,0,3,5,9,12};
-    int target = 3;
-    cout<<BinarySearch_int(nums, target);
+    // vector<int > nums = {-1,1,4,5,5,12};
+    vector<int > nums = {3, 4, 4, 7, 8, 10};
+    int target = 5;
+    // cout<<BinarySearch_int(nums, target);
+    cout<<nums[upper_bound(nums, target)];
+    cout<<nums[lower_bound(nums, target)];
+
+
 }
