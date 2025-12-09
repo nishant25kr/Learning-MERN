d0f902fefb2dfef4e11b4bc9978a6d69d2e0a715
nishant25kr <nishant927472@gmail.com>
2025-12-09 15:05:43 +0530

    Add find_element function for searching in a 2D matrix

--- DSA/Binary_search.cpp
+++ DSA/Binary_search.cpp
@@ -103,17 +103,45 @@ int cubert_floor(int n){
     return ans;
 }
 
+bool find_element(vector<vector<int >> &matrix, int target){
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    int low = 0;
+    int high = (m*n)-1;
+    
+    while(low <= high){
+        int mid = (low + high) / 2;
+        int row = mid/m;
+        int col = mid/n;
+        if(matrix[row][col] == target){
+            cout<<"["<<row<<","<<col<<"]"<<endl;
+            return true;
+        }
+        else if(matrix[row][col] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
+
 int main()
 {
-
     // vector<int > nums = {-1,1,4,5,5,12};
     vector<int> nums = {3, 4, 4, 7, 8, 10};
-    int target = 5;
+    // int target = 5;
     // cout<<BinarySearch_int(nums, target);
     // cout << nums[upper_bound(nums, target)];
     // cout << nums[lower_bound(nums, target)];
     // cout << sqrt_floor(49);
     // cout << sqrt_floor(49);
-    cout<<cubert_floor(70);
+    // cout<<cubert_floor(70);
 
+    vector<vector<int>> matrix = {
+        {1, 3, 5, 7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 60}
+    };
+    int target = 11;
+
+    cout << (find_element(matrix, target) ? "Available" : "Not available");
 }
