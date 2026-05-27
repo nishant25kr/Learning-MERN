56297a1856fe6068b6fe046a0a451250e2961900
Nishant kumar <nishant927472@gmail.com>
2026-05-27 23:15:34 +0530

    learn about function scope of var

---
+++ interview/types/one.js
@@ -0,0 +1,9 @@
+function call(input){
+    console.log("type is:",typeof input)
+}
+const x = '5'+5
+const obj = {
+    name : 'nishant'
+}
+
+call(obj)
---
+++ interview/var/one.js
@@ -0,0 +1,12 @@
+var x = 10;
+console.log(x)
+function hello(){
+    x = 20;
+    console.log('hello',x)
+}
+
+if(true){
+    x = 30;
+    console.log('two',x)
+}
+hello()
